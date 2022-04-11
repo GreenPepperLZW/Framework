a2164c2ac898bbd363d465370666f5768f200ac6
lzw <991162138@qq.com>
2022-04-11 11:01:37 +0800

    测试mybatisPlus逆向工程

---
+++ generator/src/main/java/com/lzw/generator/CodeGeneratorNew.java
@@ -0,0 +1,37 @@
+package com.lzw.generator;
+
+import com.baomidou.mybatisplus.generator.FastAutoGenerator;
+import com.baomidou.mybatisplus.generator.config.OutputFile;
+import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+
+import java.util.Collections;
+
+/**
+ * @author : lzw
+ * @date : 2022/4/11
+ * @since : 1.0
+ */
+public class CodeGeneratorNew {
+
+    public static void main(String[] args) {
+        FastAutoGenerator.create("jdbc:mysql://127.0.0.1:3306/seckill?userSSL=false", "root", "sasa")
+                .globalConfig(builder -> {
+                    builder.author("lzw") // 设置作者
+                            .enableSwagger() // 开启 swagger 模式
+                            .fileOverride() // 覆盖已生成文件
+                            .outputDir("D://"); // 指定输出目录
+                })
+                .packageConfig(builder -> {
+                    builder.parent("com.lzw") // 设置父包名
+                            .moduleName("seckill") // 设置父包模块名
+                            .pathInfo(Collections.singletonMap(OutputFile.xml, "D://")); // 设置mapperXml生成路径
+                })
+                .strategyConfig(builder -> {
+                    builder.addInclude("t_user") // 设置需要生成的表名
+                            .addTablePrefix("t_", "c_"); // 设置过滤表前缀
+                })
+                .templateEngine(new FreemarkerTemplateEngine()) // 使用Freemarker引擎模板，默认的是Velocity引擎模板
+                .execute();
+    }
+
+}
