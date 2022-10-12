015fc70e4744f7e7f4f746cc2b39b8a0949e4d9c
lzw <991162138@qq.com>
2022-10-12 17:25:23 +0800

    声明式事务测试1

---
+++ annontaion2/src/main/java/com/lzw/tx/TxConfig.java
@@ -0,0 +1,22 @@
+package com.lzw.tx;
+
+
+/*
+ *
+ * 环境搭建：
+ *  1.导入相关依赖
+ *      数据源、数据库驱动、Spring-jdbc模块
+ *  2.配置数据源、JdbcTemplate（Spring提供的简化操作数据库操作的工具）操作数据库
+ *  3.
+ *
+ */
+
+/**
+ * 声明式事务测试
+ *
+ * @author : lzw
+ * @date : 2022/10/12
+ * @since : 1.0
+ */
+public class TxConfig {
+}
