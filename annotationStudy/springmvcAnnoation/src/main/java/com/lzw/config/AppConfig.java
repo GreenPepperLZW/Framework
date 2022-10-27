2104dd22220b7c0e961c072de866acf8a122e972
lzw <991162138@qq.com>
2022-10-27 22:36:13 +0800

    自定义拦截器

--- annotationStudy/springmvcAnnoation/src/main/java/com/lzw/config/AppConfig.java
+++ annotationStudy/springmvcAnnoation/src/main/java/com/lzw/config/AppConfig.java
@@ -1,12 +1,10 @@
 package com.lzw.config;
 
+import com.lzw.MyInterceptor;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.*;
 
 /**
  * @author : lzw
@@ -33,5 +31,11 @@ public class AppConfig extends WebMvcConfigurerAdapter {
     @Override
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
         configurer.enable();
+    }
+
+    // 拦截器
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new MyInterceptor()).addPathPatterns("/**");
     }
 }
