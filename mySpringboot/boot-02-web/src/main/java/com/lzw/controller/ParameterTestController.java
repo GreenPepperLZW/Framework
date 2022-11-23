ffd5e26dd317c72fb0a25b862c81e9042f5e049e
lzw <991162138@qq.com>
2022-11-23 13:39:28 +0800

    记录参数解析执行过程

--- mySpringboot/boot-02-web/src/main/java/com/lzw/controller/ParameterTestController.java
+++ mySpringboot/boot-02-web/src/main/java/com/lzw/controller/ParameterTestController.java
@@ -1,8 +1,23 @@
 package com.lzw.controller;
 
+import org.springframework.core.MethodParameter;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.method.annotation.AbstractNamedValueMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodArgumentResolverComposite;
+import org.springframework.web.method.support.InvocableHandlerMethod;
+import org.springframework.web.method.support.ModelAndViewContainer;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.mvc.method.AbstractHandlerMethodAdapter;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
+import org.springframework.web.servlet.mvc.method.annotation.ServletInvocableHandlerMethod;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -23,6 +38,20 @@ public class ParameterTestController {
      * @PathVariable 注解使用，获取请求路径上{} 中的参数,使用@PathVariable Map<String, String> pv可以获取请求路径上携带的所有请求参数
      * @RequestHeader 获取请求头信息，@RequestHeader Map<String, String> header，map类型形参可以获取所有请求头信息
      * @CookieValue 获取cookie的值,@CookieValue("XXL_JOB_LOGIN_IDENTITY") Cookie cookie这种方式可以获取cookie的键值对
+     * <p>
+     * 参数解析执行过程：
+     * {@link DispatcherServlet#doDispatch(HttpServletRequest, HttpServletResponse)} ->
+     * {@link AbstractHandlerMethodAdapter#handle(HttpServletRequest, HttpServletResponse, Object)} ->
+     * {@link RequestMappingHandlerAdapter#handleInternal(HttpServletRequest, HttpServletResponse, HandlerMethod)} ->
+     * {@link RequestMappingHandlerAdapter#invokeHandlerMethod(HttpServletRequest, HttpServletResponse, HandlerMethod)} ->
+     * {@link ServletInvocableHandlerMethod#invokeAndHandle(ServletWebRequest, ModelAndViewContainer, Object...)} ->
+     * {@link InvocableHandlerMethod#invokeForRequest(NativeWebRequest, ModelAndViewContainer, Object...)} ->
+     * {@link InvocableHandlerMethod#getMethodArgumentValues(NativeWebRequest, ModelAndViewContainer, Object...)} ->
+     * {@link HandlerMethodArgumentResolverComposite#resolveArgument(MethodParameter, ModelAndViewContainer, NativeWebRequest, WebDataBinderFactory)} ->
+     * {@link HandlerMethodArgumentResolverComposite#getArgumentResolver(MethodParameter)}
+     * 在 getArgumentResolver 方法中会遍历循环27个参数解析器，如果参数中存在相应的注解，则会返回对应的参数解析器
+     * {@link AbstractNamedValueMethodArgumentResolver#resolveArgument(MethodParameter, ModelAndViewContainer, NativeWebRequest, WebDataBinderFactory)}
+     * 在resolveArgument方法中进行对参数进行解析
      */
     @GetMapping("/car/{id}/owner/{name}")
     public Map<String, Object> getCar(@PathVariable("id") Integer id,
@@ -82,13 +111,14 @@ public class ParameterTestController {
     /**
      * 请求路径中携带的矩阵变量名字一样时，获取参数
      * /boss/1;age=20/2;age=10
+     *
      * @param bossAge
      * @param empAge
      * @return
      */
     @GetMapping("/boss/{bossId}/{empId}")
-    public Map boss(@MatrixVariable(value = "age",pathVar = "bossId") Integer bossAge ,
-                    @MatrixVariable(value = "age",pathVar = "empId") Integer empAge) {
+    public Map boss(@MatrixVariable(value = "age", pathVar = "bossId") Integer bossAge,
+                    @MatrixVariable(value = "age", pathVar = "empId") Integer empAge) {
         Map map = new HashMap();
         map.put("bossAge", bossAge);
         map.put("empAge", empAge);
