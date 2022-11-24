ad02bfd838a12ab75ccba926fe499b944921e9b2
lzw <991162138@qq.com>
2022-11-24 16:04:18 +0800

    自定义请求参数字符转换器

--- mySpringboot/boot-02-web/src/main/java/com/lzw/config/WebConfig.java
+++ mySpringboot/boot-02-web/src/main/java/com/lzw/config/WebConfig.java
@@ -1,7 +1,11 @@
 package com.lzw.config;
 
+import com.lzw.bean.Pet;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -38,7 +42,31 @@ public class WebConfig {
                 urlPathHelper.setRemoveSemicolonContent(false);
                 configurer.setUrlPathHelper(urlPathHelper);
             }
+
+            /**
+             * 自定义请求参数字符转换器
+             * @param registry
+             */
+            @Override
+            public void addFormatters(FormatterRegistry registry) {
+                registry.addConverter(new Converter<String, Pet>() {
+                    @Override
+                    public Pet convert(String source) {
+                        // source:阿猫，5
+                        if (!StringUtils.isEmpty(source)) {
+                            String[] split = source.split(",");
+                            Pet pet = new Pet();
+                            pet.setName(split[0]);
+                            pet.setAge(Integer.parseInt(split[1]));
+                            return pet;
+                        }
+                        return null;
+                    }
+                });
+            }
         };
+
+
     }
 
 }
