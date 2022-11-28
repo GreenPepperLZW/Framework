5cd796d3776835689ae5a204f8003eb82664f415
lzw <991162138@qq.com>
2022-11-28 21:18:02 +0800

    引入thymeleaf模板引擎

---
+++ mySpringboot/boot-02-web/src/main/java/com/lzw/controller/ViewTestController.java
@@ -0,0 +1,25 @@
+package com.lzw.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * 测试试图
+ *
+ * @author : lzw
+ * @date : 2022/11/28
+ * @since : 1.0
+ */
+@Controller
+public class ViewTestController {
+
+    @GetMapping("/test01")
+    public String test01(Model model) {
+        // model中的数据会被放在请求域中 request.setAttribute("a","bb")
+        model.addAttribute("msg", "");
+        model.addAttribute("link", "https://www.baidu.com");
+        return "success";
+    }
+
+}
