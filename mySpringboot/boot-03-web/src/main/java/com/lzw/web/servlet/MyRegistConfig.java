0469d13de583be189386d5b192e76055d483f9c5
lzw <991162138@qq.com>
2022-12-09 21:52:45 +0800

    modify MyRegistConfig

--- mySpringboot/boot-03-web/src/main/java/com/lzw/web/servlet/MyRegistConfig.java
+++ mySpringboot/boot-03-web/src/main/java/com/lzw/web/servlet/MyRegistConfig.java
@@ -1,10 +1,14 @@
 package com.lzw.web.servlet;
 
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcProperties;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.DispatcherServlet;
 
 import java.util.Arrays;
 
@@ -16,14 +20,20 @@ import java.util.Arrays;
  * @since : 1.0
  */
 
-@Configuration
+@Configuration(proxyBeanMethods = true) // 保证依赖的组件是单实例的
 public class MyRegistConfig {
 
 
+    /**
+     * 注册 /my,/my02路径不会被拦截器拦截，原因是 /my，/my02的匹配精度大于 “/”，dispatcherServlet的拦截路径就是 “/”
+     * 具体配置方法：{@link DispatcherServletAutoConfiguration.DispatcherServletRegistrationConfiguration#dispatcherServletRegistration(DispatcherServlet, WebMvcProperties, ObjectProvider)}
+     *
+     * @return
+     */
     @Bean
     public ServletRegistrationBean myServlet() {
         MyServlet myServlet = new MyServlet();
-        return new ServletRegistrationBean(myServlet, "my", "my02");
+        return new ServletRegistrationBean(myServlet, "/my", "/my02");
     }
 
     @Bean
@@ -35,7 +45,7 @@ public class MyRegistConfig {
 
         // 拦截指定的路径
         FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(myFilter);
-        filterRegistrationBean.setUrlPatterns(Arrays.asList("my", "/css/*"));
+        filterRegistrationBean.setUrlPatterns(Arrays.asList("/my", "/css/*"));
         return filterRegistrationBean;
     }
 
